4811ab04b9f64e38e127569450cd8da2e2bbd6d0
hoduc <duc_ho90@yahoo.com>
2016-05-25 16:43:37 -0700

    Play Recommended Context Menu

--- YT_FF/index.js
+++ YT_FF/index.js
@@ -1,5 +1,10 @@
 var self = require("sdk/self");
 
+var cm = require("sdk/context-menu");
+cm.Item({
+    label: "Play Recommended",
+    context: cm.URLContext("youtube.com")
+});
 // a dummy function, to show how tests work.
 // to see how to test this function, look at test/test-index.js
 function dummy(text, callback) {
