35af645cd7747ac2aab0ac6efbf1cb71de62212c
hoduc <duc_ho90@yahoo.com>
2016-06-02 22:29:40 -0700

    moving toward localStorage

--- YT/YT_Safari/YT_Theater.safariextension/Scripts/playRec.js
+++ YT/YT_Safari/YT_Theater.safariextension/Scripts/playRec.js
@@ -1,5 +1,15 @@
 console.log("hello injected!");
 
+function getHrefContentTuple(nodeName)
+{
+    return nodeName.firstChild.href.split("=")[1] + "," + nodeName.firstChild.textContent;
+}
+
+function chopLastDelimeter(s, delimeter)
+{
+    delimeter = delimeter || ",";
+    return s.substring(0, s.lastIndexOf(delimeter));
+}
 function getInterestedSegment(nodeName)
 {
     var allInterestedNodes = document.getElementsByClassName("feed-item-dismissable");
@@ -19,18 +29,45 @@ function getInterestedSegment(nodeName)
     }
     else
     {
-	var ll = interestedNode.getElementsByClassName("yt-lockup-thumbnail contains-addto");//each square of videos
-	var ids = [];
-	for( var i = 0 ; i < ll.length; i++ ){
-	    ids.push( ll[i].firstChild.href.split("=")[1]);
+	//var ll = interestedNode.getElementsByClassName("yt-lockup-thumbnail contains-addto");//each square of videos
+	// var ids = [];
+	// for( var i = 0 ; i < ll.length; i++ ){
+	//     ids.push( ll[i].firstChild.href.split("=")[1]);
+	// }
+	// var test_link = "test.html?v=";
+	// for( var i = 0 ; i < ids.length; i++ ){
+	//     test_link += ids[i] + "&";
+	// }
+	// test_link = test_link.substring(0, test_link.lastIndexOf("&"));
+	// safari.self.tab.dispatchMessage("pr_data", test_link);
+	// console.log(test_link);
+	//set local storage
+
+	var ll = interestedNode.getElementsByClassName("yt-shelf-grid-item");
+	var vids = "";
+	var titles = "";
+	var users = "";
+	var user_links = "";
+	for( var i = 0; i < ll.length; i++ )
+	{
+	    var videoNode = ll[i].getElementsByClassName("yt-lockup-content");
+	    var t = getHrefContentTuple(videoNode.getElementsByClassName("yt-lockup-title contains-action-menu")).split(",");
+	    vids += t[0] + ",";
+	    titles += t[1] + ",";
+	    t = getHrefContentTuple( videoNode.getElementsByClassName("yt-lockup-byline") );
+	    users += t[0] + ",";
+	    user_links += t[1] + ",";
+	    
 	}
-	var test_link = "test.html?v=";
-	for( var i = 0 ; i < ids.length; i++ ){
-	    test_link += ids[i] + "&";
-	}
-	test_link = test_link.substring(0, test_link.lastIndexOf("&"));
-	safari.self.tab.dispatchMessage("pr_data", test_link);
-	console.log(test_link);
+	chopLastDelimeter(vids);
+	chopLastDelimeter(titles);
+	chopLastDelimeter(users);
+	chopLastDelimeter(users_links);
+	
+	localStorage.setItem("videoIds", vids);
+	localStorage.setItem("titles", titles);
+	localStorage.setItem("users", users);
+	localStorage.setItem("ulinks", users_links);
     }
 }
 
